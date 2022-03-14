136f84872ced8155720a851e25be7900d94b2e24
marie <dul.sreyneang@gmail.com>
2022-03-14 18:16:00 +0800

    Image
    replace image

--- lSpy/ViewController.swift
+++ lSpy/ViewController.swift
@@ -10,19 +10,31 @@ import UIKit
 class ViewController: UIViewController, UIScrollViewDelegate {
 
     @IBOutlet var scrollView: UIScrollView!
-    
-    var imageView = UIImageView()
+    @IBOutlet var imageView: UIImageView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        scrollView.delegate = self
-        
+        setUpScrollVew()
     }
     
-    func viewForZooming() {
+    override func viewDidAppear(_ animated: Bool) {
+        updateZoomFor(size: view.bounds.size)
+    }
+    func setUpScrollVew() {
+        scrollView.delegate = self
+    }
+    internal func viewForZooming(in scrollView: UIScrollView) -> UIView? {
+        return imageView
+    }
+    func updateZoomFor(size: CGSize) {
+        let widthScale = size.width / imageView.bounds.width
+        let heightScale = size.height / imageView.bounds.height
+        let scale = min(widthScale, heightScale)
         
+        scrollView.minimumZoomScale = scale
+        scrollView.zoomScale = scale
     }
 
-
+    
 }
 
